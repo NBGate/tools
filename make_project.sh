d53258cb211c3b516f170ccbca366ba8d7504304
maxing <xm.crazyboy@gmail.com>
2013-12-10 10:59:15 +0800

    add make_project.sh build.sh.android

---
+++ make_project.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+WORKDIR=$(cd "$(dirname "$0")";cd ..;pwd)
+COCOS2DX_DIR=$WORKDIR/cocos2d-x
+COCOS2DX_APP_DIR=$COCOS2DX_DIR/cocos2dx/platform/android/java
+CREATOR_SCRIPT_DIR=$COCOS2DX_DIR/tools/project-creator
+SCRIPT=$CREATOR_SCRIPT_DIR/create_project.py
+
+PROJECT=
+TARGET=android-10
+while getopts "p:t:" OPTION; do
+    case "$OPTION" in
+        p)
+            PROJECT=$(echo $OPTARG)
+            ;;
+        t)
+            TARGET=$(echo $OPTARG)
+            ;;
+        ?)
+            ;;
+    esac
+done
+
+cd $CREATOR_SCRIPT_DIR
+PACKAGE="game.nbgate.$PROJECT"
+LANG=cpp
+$SCRIPT -project $PROJECT -package $PACKAGE -language $LANG
+if [ $? -ne 0 ]; then exit 1;fi
+cd -
+
+PROJECT_DIR=$COCOS2DX_DIR/projects/$PROJECT
+TARGETPROJECT_DIR=$WORKDIR/$PROJECT
+mv $PROJECT_DIR $TARGETPROJECT_DIR
+if [ $? -ne 0 ]; then echo "error: mv project from $PROJECT_DIR to target path $TARGETPROJECT_DIR failed";exit 1;fi
+
+APPDIR=$TARGETPROJECT_DIR/proj.android
+cd $APPDIR
+sed -i 's/COCOS2DX_ROOT="$DIR\/..\/..\/.."/COCOS2DX_ROOT="$DIR\/..\/..\/cocos2d-x"/' $APPDIR/build_native.sh
+grep 'DIR/../../cocos2d-x' $APPDIR/build_native.sh > /dev/null
+if [ $? -ne 0 ]; then echo "error: rewrite COCOS2DX_ROOT in build_native.sh failed.";exit 1;fi
+sed -i 's/..\/..\/..\/cocos2dx/..\/..\/cocos2d-x\/cocos2dx/' $APPDIR/project.properties
+grep '../../cocos2d-x' $APPDIR/project.properties > /dev/null
+if [ $? -ne 0 ]; then echo "error: rewrite android.library.reference.1 failed in file project.properties.";exit 1;fi
+
+if [ -f $WORKDIR/tools/build.sh.android ]; then cp $WORKDIR/tools/build.sh.android $PROJECT_DIR/build.sh
+
+android update project -n $PROJECT -p $APPDIR -t $TARGET
+
